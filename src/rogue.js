f1af5e9f87d086766a410a5e858eb4342e8d710e
Andy <astubbs50@gmail.com>
2026-01-28 04:24:07 -0700

    Updates to map generation

--- src/rogue.js
+++ src/rogue.js
@@ -1,16 +1,32 @@
+"use strict";
+
 $.screen( "640x350" );
-const rooms = createRooms();
-for( const room of rooms ) {
-	for( let y = room.y; y < room.y + room.h; y += 1 ) {
-		for( let x = room.x; x < room.x + room.w; x += 1 ) {
-			$.setPos( x, y );
-			$.print( "X", true );
-		}
+
+const m_map = [];
+const m_rooms = [];
+const m_width = $.getCols();
+const m_height = $.getRows();
+
+// Create an empty map
+for( let y = 0; y < m_height; y++ ) {
+	m_map[ y ] = [];
+	for( let x = 0; x < m_width; x++ ) {
+		m_map[ y ][ x ] = TILE_BLANK;
+	}
+}
+
+createRooms();
+
+// Render the map
+for( let y = 0; y < m_map.length; y += 1 ) {
+	for( let x = 0; x < m_map[ y ].length; x += 1 ) {
+		const tile = m_map[ y ][ x ];
+		$.setPos( x, y );
+		$.print( tile, true );
 	}
 }
 
 function createRooms() {
-	const rooms = [];
 	const roomMinSize = 3;
 	const roomMaxWidth = 12;
 	const roomMaxHeight = 8;
@@ -21,15 +37,16 @@ function createRooms() {
 			const newRoom = {};
 			newRoom.w = randomRange( roomMinSize, roomMaxWidth );
 			newRoom.h = randomRange( roomMinSize, roomMaxHeight );
-			newRoom.x = randomRange( 1, $.getCols() - newRoom.w - 1 );
-			newRoom.y = randomRange( 1, $.getRows() - newRoom.h - 1 );
-			isRoomPlaced = !rooms.some( room => isRoomOverlapping( newRoom, room ) );
+			newRoom.x = randomRange( 1, m_width - newRoom.w - 1 );
+			newRoom.y = randomRange( 1, m_height - newRoom.h - 1 );
+			isRoomPlaced = !m_rooms.some( room => isRoomOverlapping( newRoom, room ) );
 			if( isRoomPlaced ) {
-				rooms.push( newRoom );
+				m_rooms.push( newRoom );
+				carveRoom( newRoom );
 			}
 		}
 	}
-	return rooms;
+	return m_rooms;
 }
 
 function isRoomOverlapping( room1, room2 ) {
@@ -39,6 +56,34 @@ function isRoomOverlapping( room1, room2 ) {
 		room1.y + room1.h + 1 < room2.y ||
 		room1.y > room2.y + room2.h + 1
 	);
+}
+
+function carveRoom( room ) {
+	for( let y = room.y; y < room.y + room.h; y++ ) {
+		for ( let x = room.x; x < room.x + room.w; x++ ) {
+			m_map[ y ][ x ] = TILE_FLOOR;
+		}
+	}
+};
+
+function connectRooms( room1, room2 ) {
+
+}
+
+function carveH( map, x1, x2, y ) {
+	for( let x = Math.min( x1, x2 ); x <= Math.max( x1, x2 ); x++ ) {
+		if( map[ y ][ x ] === TILE_BLANK ) {
+			map[ y ][ x ] = TILE_PATH;
+		}
+	}
+}
+
+function carveV( map, y1, y2, x ) {
+	for( let y = Math.min( y1, y2 ); y <= Math.max( y1, y2 ); y++ ) {
+		if( map[ y ][ x ] === TILE_BLANK ) {
+			map[ y ][ x ] = TILE_PATH;
+		}
+	}
 }
 
 function randomRange( min, max ) {
---
+++ src/tiles.js
@@ -0,0 +1,19 @@
+"use strict";
+// File: tiles.js
+
+const TILE_BLANK = " ";
+const TILE_FLOOR = "X";
+const TILE_DOOR = String.fromCharCode( 240 );
+const TILE_WALL_NW = String.fromCharCode( 201 );
+const TILE_WALL_N = String.fromCharCode( 205 );
+const TILE_WALL_NE = String.fromCharCode( 187 );
+const TILE_WALL_W = String.fromCharCode( 186 );
+const TILE_WALL_E = String.fromCharCode( 186 );
+const TILE_WALL_SW = String.fromCharCode( 200 );
+const TILE_WALL_S = String.fromCharCode( 205 );
+const TILE_WALL_SE = String.fromCharCode( 188 );
+//const TILE_PATH = String.fromCharCode( 176 );
+const TILE_PATH = "o";
+const WALKABLE = [
+	TILE_FLOOR, TILE_DOOR, TILE_PATH
+];
